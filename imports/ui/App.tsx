8515cc6a42e4393a3032f233e4f91914cd68dbfa
alex olivares <alexander.b.olivares@gmail.com>
2023-05-08 21:16:53 -0500

    first commit. bootstrap meteor app with ts

---
+++ imports/ui/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from "./LoginForm"
+import Profile from "./Profile"
+
+export const App = () => (
+
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <LoginForm />
+    <Profile />
+  </div>
+);
---
+++ imports/ui/Profile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function User() {
+  // use tracker to get this stuff?
+
+  return (
+    <div>
+      <h1> Profile </h1>
+      {/* <h1>Username: {username}</h1>
+      <p>Login Time: {timestamp}</p> */}
+    </div>
+  );
+}
+
+export default User;
