7303b84d9e3b40adca9d20bc33a3d9a8a222e6b7
alex olivares <alexander.b.olivares@gmail.com>
2024-01-11 21:26:20 -0600

    extend login token expiration

--- server/main.ts
+++ server/main.ts
@@ -4,10 +4,14 @@ import "../imports/api/methods/tasksMethods";
 import "../imports/api/methods/listsMethods";
 import "../imports/api/publications";
 import "./emailTemplates";
+import { Accounts } from "meteor/accounts-base";
 
 Meteor.startup(async () => {
   console.log("Server startup");
 
   const { host, username, password, port } = Meteor.settings.private.smtp;
   process.env.MAIL_URL = `smtp://${username}:${password}@${host}:${port}`;
+  Accounts.config({
+    loginExpirationInDays: 30,
+  });
 });
